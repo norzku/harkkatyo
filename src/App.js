c58a331bab81ad196b00ff40d7c4addb7206c33a
Miika Husso <Miika.Husso@edu.savonia.fi>
2022-04-27 16:46:34 +0300

    Appi mukaan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Asiakaslista3 from './tehtävä19';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          Toimiiko tämä
-        </a>
+        <Asiakaslista3/>
       </header>
     </div>
   );
